c4832bab5b34589da97b700f89bd3dd033069e4d
Tommy M. McGuire <mcguire@crsr.net>
2013-03-31 10:56:45 -0500

    Clean up, formatting

--- anagrams-vectors-wide.rs
+++ anagrams-vectors-wide.rs
@@ -3,13 +3,14 @@ extern mod std;
 mod combinations;
 mod bisect;
 
+use core::comm::*;
 use core::io::*;
 use core::hashmap::linear::*;
 use core::cmp::{Eq, Ord};
 use core::task::*;
 
 struct kv_pair {
-    keys : ~[~[int]],
+    keys   : ~[~[int]],
     values : ~[~[~str]]
 }
 
@@ -17,9 +18,9 @@ fn load_dictionary(width : uint) -> ~[kv_pair] {
     match file_reader(&Path("anadict-rust.txt")) {
         Ok(reader) => {
             let mut t = 0;
-            let mut pairs =
-                vec::from_fn(width,
-                             |_| kv_pair{keys : ~[], values : ~[]});
+            let mut pairs = vec::from_fn(width, 
+                                         |_| kv_pair{keys   : ~[],
+                                                     values : ~[]});
             for reader.each_line() |line| {
                 let words = line.split_str(" ");
                 pairs[t].keys.push(
@@ -42,34 +43,46 @@ fn get_letters(s : &str) -> ~[int] {
     return vec::from_fn(t.len(), |i| t[i] as int);
 }
 
+fn search(argument : &[int], keys : &[~[int]], values : &[~[~str]]) -> ~LinearSet<~str> {
+    let klen = keys.len();
+    let mut set = ~LinearSet::new();
+    for uint::range(2, argument.len() + 1) |i| {
+        let mut key = vec::from_elem(i, 0);
+        for combinations::each_combination(argument,i) |combo| {
+            for uint::range(0,i) |j| { key[j] = combo[j]; }
+            let j = bisect::bisect_left_ref(keys, &key, 0, klen);
+            if j < klen && keys[j] == key {
+                for values[j].each |&word| { set.insert(word); }
+            }
+        }
+    }
+    return set;
+}
+
 fn main() {
     let width = 6;
 
     let args = os::args();
-    if args.len() < 2 {
-        fail!(~"Usage: anagrams letters");
-    }
+    if args.len() < 2 { fail!(~"Usage: anagrams letters"); }
     let letters = get_letters(args[1]);
 
-    for load_dictionary(width).each |&kv_pair{keys : keys, values : values}| {
+    let (response_port,response_chan)
+        : (Port<~LinearSet<~str>>,Chan<~LinearSet<~str>>) = stream();
+    let response_chan = SharedChan(response_chan);
 
-        let argument = copy letters;
-
+    for load_dictionary(width).each |&kv_pair{keys   : keys,
+                                              values : values}| {
+        let response_chan = response_chan.clone();
+        let letters = copy letters;
         do spawn {
-            let klen = keys.len();
-            let mut set : LinearSet<~str> = LinearSet::new();
-            for uint::range(2, argument.len() + 1) |i| {
-                let mut key = vec::from_elem(i, 0);
-                for combinations::each_combination(argument,i) |combo| {
-                    for uint::range(0,i) |j| { key[j] = combo[j]; }
-                    let j = bisect::bisect_left_ref(keys, &key, 0, klen);
-                    if j < klen && keys[j] == key {
-                        for values[j].each |&word| { set.insert(word); }
-                    }
-                }
-            }
-            println(fmt!("%u", set.len()));
+            let set = search(letters, keys, values);
+            response_chan.send(set);
         }
-
     }
+    let mut set = ~LinearSet::new();
+    for uint::range(0,width) |_| {
+        let res = response_port.recv();
+        for res.each |&word| { set.insert(word); }
+    }
+    println(fmt!("%u", set.len()));
 }
