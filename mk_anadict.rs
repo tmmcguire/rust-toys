b7393bb0cfef6a62807b8b7e2c483e98d00f39bf
Tommy M. McGuire <mcguire@crsr.net>
2013-04-01 18:00:42 -0500

    Updated to rust 0.6 (etc.)

--- mk_anadict.rs
+++ mk_anadict.rs
@@ -1,11 +1,11 @@
 extern mod std;
 
-use io::*;
-use result::*;
-use std::map::*;
+use core::io::*;
+use core::result::*;
+use core::hashmap::linear::*;
 
-fn read_dict(reader : Reader) -> ~HashMap<@~str,@[@~str]> {
-    let map = ~HashMap();
+fn read_dict(reader : Reader) -> ~LinearMap<~str,~[~str]> {
+    let mut map = ~LinearMap::new();
     for reader.each_line |line| {
         let line   = line.trim();
         let length = line.len();
@@ -14,23 +14,30 @@ fn read_dict(reader : Reader) -> ~HashMap<@~str,@[@~str]> {
             let mut chars = str::chars(line);
             std::sort::quick_sort(chars, |a,b| *a <= *b);
             let key = str::from_chars(chars);
-            map.update(@key, @[@line], |old,nw| at_vec::append(old,nw));
+            
+            let mut value = match map.pop(&key) {
+                None => { ~[] }
+                Some(old) => { old }
+            };
+            value.push(line);
+            map.insert(key,value);
+
         }
     }
     return map;
 }
 
-fn sorted_keys<V:Copy>(dict : &HashMap<@~str,V>) -> ~[@~str] {
-    let mut keys = vec::with_capacity( dict.size() );
-    for dict.each_key |key| { keys.push(key); }
+fn sorted_keys<V:Copy>(dict : &LinearMap<~str,V>) -> ~[~str] {
+    let mut keys = vec::with_capacity( dict.len() );
+    for dict.each_key |&key| { keys.push(key); }
     std::sort::quick_sort(keys, |a,b| *a <= *b);
     return keys;
 }
 
-fn print_dict(writer : Writer, dict : &HashMap<@~str,@[@~str]>) {
+fn print_dict(writer : Writer, dict : &LinearMap<~str,~[~str]>) {
     for sorted_keys(dict).each |key| {
-        let line = str::connect( dict.get(*key).map(|v| **v), " " );
-        writer.write_str( fmt!("%s %s\n", **key, line) );
+        let line = str::connect( dict.get(key).map(|&v| v), " " );
+        writer.write_str( fmt!("%s %s\n", *key, line) );
     }
 }
 
@@ -38,7 +45,7 @@ fn main() {
     match (file_reader(&Path("/usr/share/dict/words")),
            file_writer(&Path("anadict-rust.txt"), [Create,Truncate])) {
         (Ok(r),    Ok(w))    => { print_dict(w, read_dict(r)); }
-        (Err(msg), _)        => { fail msg; }
-        (_,        Err(msg)) => {fail msg; }
+        (Err(msg), _)        => { fail!(msg); }
+        (_,        Err(msg)) => {fail!(msg); }
     }
 }
