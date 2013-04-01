79d18ad810dc9bb35d9ad14d6aa780bf03aee2d8
Tommy M. McGuire <mcguire@crsr.net>
2013-04-01 11:35:49 -0500

    clean up

--- anagrams-vectors-tasks.rs
+++ anagrams-vectors-tasks.rs
@@ -32,6 +32,8 @@ fn get_letters(s : &str) -> ~[int] {
 }
 
 fn main() {
+    let width = 6;
+    let depth = 10000;
 
     let args = os::args();
     if args.len() < 2 {
@@ -39,53 +41,60 @@ fn main() {
     }
     let letters = get_letters(args[1]);
 
-    let (response_port,response_chan) : (Port<~LinearSet<~str>>,Chan<~LinearSet<~str>>) = stream();
+    let (keys,values) = load_dictionary();
+
+    let (response_port,response_chan)
+        : (Port<~LinearSet<~str>>,Chan<~LinearSet<~str>>) = stream();
     let response_chan = SharedChan(response_chan);
 
-    let request_streams   : ~[(Port<~[int]>,Chan<~[int]>)] = vec::from_fn(6, |_| stream());
-    let mut request_chans : ~[Chan<~[int]>]                = ~[];
+    let request_streams   : ~[(Port<~[~[int]]>,Chan<~[~[int]]>)]
+        = vec::from_fn(width, |_| stream());
+    let mut request_chans : ~[Chan<~[~[int]]>] = ~[];
     for request_streams.each |&(request_port, request_chan)| {
-
         request_chans.push(request_chan);
         let response_chan = response_chan.clone();
-
+        let keys = copy keys;
+        let values = copy values;
         do spawn {
-
-            let (keys,values) = load_dictionary();
             let klen = keys.len();
-            let mut set : ~LinearSet<~str> = ~LinearSet::new();
-
+            let mut set = ~LinearSet::new();
             loop {
-                let key = request_port.recv();
-                if key.len() == 0 { break; }
-                let j = bisect::bisect_left_ref(keys, &key, 0, klen);
-                if j < klen && keys[j] == key {
-                    for values[j].each |&word| {
-                        set.insert(copy word);
+                let key_set = request_port.recv();
+                if key_set.len() == 0 { break; }
+                for key_set.each |key| {
+                    let j = bisect::bisect_left_ref(keys, key, 0, klen);
+                    if j < klen && keys[j] == *key {
+                        for values[j].each |&word| {
+                            set.insert(copy word);
+                        }
                     }
                 }
             }
-            println(fmt!("    %u", set.len()));
             response_chan.send(set);
         }
     }
 
     let mut t = 0;
+    let mut key_set = ~[];
     for uint::range(2,letters.len() + 1) |i| {
-        // let mut key = vec::from_elem(i, 0);
         for combinations::each_combination(letters,i) |combo| {
-            // for uint::range(0,i) |j| { key[j] = combo[j]; }
             let key = vec::from_fn(i, |i| combo[i]);
-            request_chans[t].send(key);
-            t = (t + 1) % 6;
+            key_set.push(key);
+            if key_set.len() >= depth {
+                let mut ks = ~[];
+                ks <-> key_set;
+                request_chans[t].send(ks);
+                t = (t + 1) % width;
+            }
         }
     }
+    if !key_set.is_empty() { request_chans[t].send(key_set); }
     for request_chans.each |chan| { chan.send(~[]) };
+
     let mut set : ~LinearSet<~str> = ~LinearSet::new();
-    for uint::range(0,6) |_| {
+    for uint::range(0,width) |_| {
         let res = response_port.recv();
         for res.each |&word| { set.insert(word); }
     }
     println(fmt!("%u", set.len()));
-
 }
