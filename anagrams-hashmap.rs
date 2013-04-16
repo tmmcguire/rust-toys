d585c48a29011fa257eeaaa86f67d999b245a530
Tommy M. McGuire <mcguire@crsr.net>
2013-04-16 17:12:33 -0500

    Use i8 instead of int

--- anagrams-hashmap.rs
+++ anagrams-hashmap.rs
@@ -1,19 +1,18 @@
 extern mod std;
-
-mod combinations;
-mod bisect;
+extern mod misc;
+extern mod combinations;
 
 use core::io::*;
 use core::hashmap::linear::*;
 
-fn load_dictionary() -> ~LinearMap<~[int],~[~str]> {
+fn load_dictionary() -> ~LinearMap<~[i8],~[~str]> {
     match file_reader(&Path("anadict-rust.txt")) {
         Ok(reader) => {
             let mut map = ~LinearMap::new();
             for reader.each_line |line| {
-                let words = line.split_str(" ");
-                let key = str::chars(words[0]);
-                map.insert(vec::from_fn(key.len(), |i| key[i] as int),
+                let words = misc::split_words(line);
+                let key = str::to_chars(words[0]);
+                map.insert(vec::from_fn(key.len(), |i| key[i] as i8),
                            vec::from_fn(words.len() - 1, |i| copy words[i+1]));
             }
             return map;
@@ -22,20 +21,17 @@ fn load_dictionary() -> ~LinearMap<~[int],~[~str]> {
     }
 }
 
-fn get_letters(s : &str) -> ~[int] {
-    let mut t = str::chars(s);
+fn get_letters(s : &str) -> ~[i8] {
+    let mut t = str::to_chars(s);
     std::sort::quick_sort(t, |a,b| *a <= *b);
-    return vec::from_fn(t.len(), |i| t[i] as int);
+    return vec::from_fn(t.len(), |i| t[i] as i8);
 }
 
-fn main() {
-    let args = os::args();
-    if args.len() < 2 {
-        fail!(~"Usage: anagrams letters");
-    }
-    let letters = get_letters(args[1]);
-    let dictionary = load_dictionary();
-    let mut set = LinearSet::new();
+fn search(letters : &[i8],
+          dictionary : &LinearMap<~[i8],~[~str]>)
+    -> ~LinearSet<~str>
+{
+    let mut set = ~LinearSet::new();
     for uint::range(2, letters.len() + 1) |i| {
         let mut key = vec::from_elem(i, 0);
         for combinations::each_combination(letters,i) |combo| {
@@ -49,5 +45,16 @@ fn main() {
             }
         }
     }
+    return set;
+}
+
+fn main() {
+    let args = os::args();
+    if args.len() < 2 {
+        fail!(~"Usage: anagrams letters");
+    }
+    let letters = get_letters(args[1]);
+    let dictionary = load_dictionary();
+    let set : ~LinearSet<~str> = search(letters,dictionary);
     println(fmt!("%u", set.len()));
 }
