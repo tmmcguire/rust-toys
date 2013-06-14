b5dbb6c680dc6ed5ebd7e78913640bfc2f60ece0
Tommy M. McGuire <mcguire@crsr.net>
2013-06-14 17:19:20 -0500

    Add hashmap/mmap version

---
+++ anagrams-hashmap-mmap.rs
@@ -0,0 +1,68 @@
+extern mod extra;
+
+extern mod combinations;
+extern mod mmap;
+
+use std::hashmap::*;
+
+fn get_letters(s : &str) -> ~[u8] {
+    let mut t = str::to_chars(s);
+    extra::sort::quick_sort(t, |a,b| *a <= *b);
+    return vec::from_fn(t.len(), |i| t[i] as u8);
+}
+
+fn line_map<'b>(buffer : &'b [u8]) -> HashMap<&'b [u8],&'b [u8]> {
+    let length = buffer.len();
+    let mut map = HashMap::new();
+    let mut i = 0;
+    while i < length {
+        let mut j = i;
+        while j < length && buffer[j] != ' ' as u8 { j += 1; }
+        let mut k = j+1;
+        while k < length && buffer[k] != '\n' as u8 { k += 1; }
+        map.insert(vec::slice(buffer, i, j), vec::slice(buffer, j+1, k));
+        i = k + 1;
+    }
+    return map;
+}
+
+#[deriving(IterBytes)]
+struct MapKey(~[u8]);
+
+impl<'self> Equiv<&'self [u8]> for MapKey {
+    fn equiv(&self, other: & &'self [u8]) -> bool {
+        let slice1: &[u8] = **self;
+        let slice2: &[u8] = *other;
+        slice1 == slice2
+    }
+}
+
+fn search<'b>(letters : &[u8], dictionary : &'b HashMap<&'b [u8],&'b [u8]>) -> HashSet<&'b [u8]>
+{
+    let mut set = HashSet::new();
+    for uint::range(2, letters.len() + 1) |i| {
+        let mut key = MapKey(vec::from_elem(i, 0));
+        for combinations::each_combination(letters,i) |combo| {
+            for combo.eachi |j,&ch| { key[j] = ch; }
+            dictionary.find_equiv(&key).map(|&v| set.insert(*v));
+        }
+    }
+    return set;
+}
+
+fn main() {
+    let args = os::args();
+    if args.len() < 2 {
+        fail!(~"Usage: anagrams letters");
+    }
+    let letters = get_letters(args[1]);
+    do mmap::with_mmap_file_contents("anadict-rust.txt") |buf| {
+        let map = line_map(buf);
+        let set = search(letters, &map);
+        let mut count = 0;
+        for set.each |ln| {
+            count += 1 + vec::count(*ln, &(' ' as u8));
+        }
+        println(fmt!("%?", count));
+    }
+}
