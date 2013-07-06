58107200def246d4119608ec309b0177e2d35225
Tommy M. McGuire <mcguire@crsr.net>
2013-07-05 19:59:57 -0500

    Initial version, comparing SipHash with djbhash

---
+++ hashing-performance.rs
@@ -0,0 +1,49 @@
+extern mod extra;
+
+use extra::time;
+
+fn duration(f : &fn () -> ()) -> time::Timespec {
+    let start = time::get_time();
+    f();
+    let end = time::get_time();
+    let d_sec = end.sec - start.sec;
+    let d_nsec = end.nsec - start.nsec;
+    if d_nsec >= 0 {
+        time::Timespec { sec : d_sec, nsec : d_nsec }
+    } else {
+        time::Timespec { sec : d_sec - 1, nsec : d_nsec + 1000000000 }
+    }
+}
+    
+fn thirtythree_million_siphashes() {
+    use std::hash::*;
+
+    let s = ~"abcdefghijklmnopqrstuvwxyz";
+    let mut potato = 0u64;
+    for std::uint::range(0, 33000000) |_| {
+        potato ^= s.hash();
+    }
+    println(fmt!("%?", potato));
+}
+
+fn djbhash(bytes : &[u8]) -> u64 {
+    let mut hash = 5381u64;
+    for bytes.iter().advance |byte| {
+        hash = (33u64 * hash) ^ *byte as u64;
+    }
+    return hash;
+}
+    
+fn thirtythree_million_djbhashes() {
+    let s = ~"abcdefghijklmnopqrstuvwxyz";
+    let mut potato = 0u64;
+    for std::uint::range(0, 33000000) |_| {
+        potato ^= djbhash(s.as_bytes());
+    }
+    println(fmt!("%?", potato));
+}
+
+fn main() {
+    println(fmt!("sip: %?", duration(thirtythree_million_siphashes)));
+    println(fmt!("djb: %?", duration(thirtythree_million_djbhashes)));
+}
