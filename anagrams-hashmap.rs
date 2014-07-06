9632111304bcc3c686fac665628aa2de369faad1
Tommy M. McGuire <mcguire@crsr.net>
2014-07-06 15:25:17 -0500

    update to rust 0.11

--- anagrams-hashmap.rs
+++ anagrams-hashmap.rs
@@ -1,11 +1,9 @@
-extern mod extra;
-extern mod combinations;
+extern crate combinations;
 
-use std::{vec,iter,os};
-use std::io::File;
-use std::io::buffered::BufferedReader;
+use std::{iter,os};
+use std::io::{File,BufferedReader};
 use std::path::Path;
-use std::hashmap::{HashMap, HashSet};
+use std::collections::hashmap::{HashMap, HashSet};
 // use extra::time;
 
 // fn duration(tag : &str, start : time::Timespec, end : time::Timespec) {
@@ -18,36 +16,38 @@ use std::hashmap::{HashMap, HashSet};
 //     }
 // }
 
-pub fn split_words(s : &str) -> ~[~str] { s.words().map(|w| w.to_owned()).collect() }
+pub fn split_words(s : &str) -> Vec<String> { s.words().map(|w| w.to_string()).collect() }
 
-fn load_dictionary() -> ~HashMap<~[i8],~[~str]> {
+fn load_dictionary() -> HashMap<Vec<i8>,Vec<String>> {
     let path = Path::new("anadict.txt");
     let file = File::open(&path);
     let mut bufferedFile = BufferedReader::new(file);
-    let mut map = ~HashMap::new();
+    let mut map = HashMap::new();
     for line in bufferedFile.lines() {
-        let words = split_words(line);
-        let key : ~[char] = words[0].chars().collect();
-        map.insert(vec::from_fn(key.len(), |i| key[i] as i8),
-                   vec::from_fn(words.len() - 1, |i| words[i+1].clone()));
+        let line = line.unwrap();
+        let words = split_words(line.as_slice());
+        let key : Vec<char> = words.get(0).as_slice().chars().collect();
+        map.insert(Vec::from_fn(key.len(), |i| *key.get(i) as i8),
+                   Vec::from_fn(words.len() - 1, |i| words.get(i+1).clone()));
     }
     return map;
 }
 
-fn get_letters(s : &str) -> ~[i8] {
-    let mut t : ~[char] = s.chars().collect();
+fn get_letters(s : &str) -> Vec<i8> {
+    let mut t : Vec<char> = s.chars().collect();
     t.sort();
-    return vec::from_fn(t.len(), |i| t[i] as i8);
+    return Vec::from_fn(t.len(), |i| *t.get(i) as i8);
 }
 
-fn search(letters : &[i8], dictionary : &HashMap<~[i8],~[~str]>) -> ~HashSet<~str>
+fn search(letters : &[i8], dictionary : &HashMap<Vec<i8>,Vec<String>>) -> HashSet<String>
 {
-    let mut set = ~HashSet::new();
+    let mut set = HashSet::new();
     for i in iter::range(0, letters.len() + 1) {
         // let start = time::get_time();
-        let mut key : ~[i8] = vec::from_elem(i, 0i8);
+        let mut key : Vec<i8> = Vec::from_elem(i, 0i8);
         combinations::each_combination(letters, i, |combo| {
-            for j in iter::range(0, combo.len()) { key[j] = combo[j]; }
+            let combo : &[i8] = combo;
+            for j in iter::range(0, combo.len()) { *key.get_mut(j) = combo[j]; }
             match dictionary.find(&key) {
                 Some(ref val) => {
                     for word in val.iter() { set.insert(word.clone()); }
@@ -63,12 +63,12 @@ fn search(letters : &[i8], dictionary : &HashMap<~[i8],~[~str]>) -> ~HashSet<~st
 fn main() {
     // let start = time::get_time();
     let args = os::args();
-    if args.len() < 2 { fail!(~"Usage: anagrams letters"); }
-    let letters = get_letters(args[1]);
+    if args.len() < 2 { fail!("Usage: anagrams letters"); }
+    let letters = get_letters(args.get(1).as_slice());
     // duration("get_letters", start, time::get_time());
     let dictionary = load_dictionary();
     // duration("load_dictionary", start, time::get_time());
-    let set = search(letters,dictionary);
+    let set = search(letters.as_slice(),&dictionary);
     // duration("search", start, time::get_time());
     println!("{:u}", set.len());
 }
--- combinations.rs
+++ combinations.rs
@@ -1,9 +1,5 @@
-#[ crate_id = "combinations#1.0" ];
-#[ crate_type = "lib" ];
-
-extern mod std;
-
-use std::vec;
+#![ crate_id = "combinations#1.0" ]
+#![ crate_type = "lib" ]
 
 /// Iterate over `r`-length subsequences of elements from `values`.
 ///
@@ -36,145 +32,146 @@ pub fn each_combination<T:Clone>(values : &[T], r : uint, fun : |combo:&[T]| -> 
     let length          = values.len();
     if r == 0 || r > length { return; }
     let max_indices0    = length - r;
-    let mut indices     = vec::from_fn(r, |i| i);
-    let mut combination = vec::from_fn(r, |i| values[i].clone());
+    let mut indices     = Vec::from_fn(r, |i| i);
+    let mut combination = Vec::from_fn(r, |i| values[i].clone());
     loop {
-        fun(combination);
+        fun(combination.as_slice());
         // Increment the indices
         let mut i = r - 1;
-        indices[i] += 1;
-        while i > 0 && indices[i] > max_indices0 + i {
+        *indices.get_mut(i) = indices.get(i) + 1;
+        while i > 0 && *indices.get(i) > max_indices0 + i {
             // indices[i] now too large; decrement i, increment indices[i]
             // and we'll fix up the following indices later
             i -= 1;
-            indices[i] += 1;
+            *indices.get_mut(i) = indices.get(i) + 1;
         }
         // Can't fix up 'done'
-        if indices[0] > max_indices0 { break; }
-        // Fix up the indices and the combination from i to r-1
-        combination[i] = values[indices[i]].clone();
-        for i in range(i + 1, r) {
-            indices[i] = indices[i-1] + 1;
-            combination[i] = values[indices[i]].clone();
-        }
-    }
-    return;
-}
+        if *indices.get(0) > max_indices0 { break; }
+         // Fix up the indices and the combination from i to r-1
+         *combination.get_mut(i) = values[*indices.get(i)].clone();
+         for i in range(i + 1, r) {
+             *indices.get_mut(i) = indices.get(i-1) + 1;
+             *combination.get_mut(i) = values[*indices.get(i)].clone();
+         }
+     }
+     return;
+ }
 
-/// Iterate over `r`-length subsequences of elements from `values`.
-///
-/// This is an alternative to each_combination that uses references to
-/// avoid copying the elements of the values vector.
-/// 
-/// To avoid memory allocations and copying, the iterator will be
-/// passed a reference to a vector containing references to the
-/// elements in the original `values` vector.
-///
-/// # Arguments
-///
-/// * `values` - A vector of values from which the combinations are
-/// chosen
-///
-/// * `r` - The length of the emitted combinations
-///
-/// * `fun` - The function to iterate over the combinations
-#[inline]
-pub fn each_combination_ref<'v,T>(values : &'v [T], r : uint, fun : |combo:&[&'v T]| -> ()) {
-    each_combination(vec::from_fn(values.len(), |i| &values[i]), r, fun);
-}
+ /// Iterate over `r`-length subsequences of elements from `values`.
+ ///
+ /// This is an alternative to each_combination that uses references to
+ /// avoid copying the elements of the values vector.
+ /// 
+ /// To avoid memory allocations and copying, the iterator will be
+ /// passed a reference to a vector containing references to the
+ /// elements in the original `values` vector.
+ ///
+ /// # Arguments
+ ///
+ /// * `values` - A vector of values from which the combinations are
+ /// chosen
+ ///
+ /// * `r` - The length of the emitted combinations
+ ///
+ /// * `fun` - The function to iterate over the combinations
+ #[inline]
+ pub fn each_combination_ref<'v,T>(values : &'v [T], r : uint, fun : |combo:&[&'v T]| -> ()) {
+     let v = Vec::from_fn(values.len(), |i| &values[i]);
+     each_combination(v.as_slice(), r, fun);
+ }
 
-/// Reverse a slice of a vector in place.
-///
-/// Reverse the elements in the vector between `start` and `end - 1`.
-///
-/// # Arguments
-///
-/// * `v` - The mutable vector to be modified
-///
-/// * `start` - Index of the first element of the slice
-///
-/// * `end` - Index one past the final element to be reversed.
-///
-/// # Example
-///
-/// Assume a mutable vector `v` contains `[1,2,3,4,5]`. After the call:
-///
-/// ~~~
-///
-/// reverse_part(v, 1, 4);
-///
-/// ~~~
-///
-/// `v` now contains `[1,4,3,2,5]`.
-///
-/// # Safety note
-///
-/// Behavior is undefined if `start` or `end` do not represent valid
-/// positions in `v`.
-pub fn reverse_part<T>(v : &mut [T], start : uint, end : uint) {
-    let mut i = start;
-    let mut j = end - 1;
-    while i < j {
-        // v[i] <-> v[j];
-        // vec::swap(v,i,j);
-        v.swap(i,j);
-        i += 1;
-        j -= 1;
-    }
-}
+ /// Reverse a slice of a vector in place.
+ ///
+ /// Reverse the elements in the vector between `start` and `end - 1`.
+ ///
+ /// # Arguments
+ ///
+ /// * `v` - The mutable vector to be modified
+ ///
+ /// * `start` - Index of the first element of the slice
+ ///
+ /// * `end` - Index one past the final element to be reversed.
+ ///
+ /// # Example
+ ///
+ /// Assume a mutable vector `v` contains `[1,2,3,4,5]`. After the call:
+ ///
+ /// ~~~
+ ///
+ /// reverse_part(v, 1, 4);
+ ///
+ /// ~~~
+ ///
+ /// `v` now contains `[1,4,3,2,5]`.
+ ///
+ /// # Safety note
+ ///
+ /// Behavior is undefined if `start` or `end` do not represent valid
+ /// positions in `v`.
+ pub fn reverse_part<T>(v : &mut [T], start : uint, end : uint) {
+     let mut i = start;
+     let mut j = end - 1;
+     while i < j {
+         // v[i] <-> v[j];
+         // vec::swap(v,i,j);
+         v.swap(i,j);
+         i += 1;
+         j -= 1;
+     }
+ }
 
-/// Iterate over all permutations of vector `values`.
-///
-/// Permutations are produced in lexicographic order with respect to
-/// the order of elements in `values` (so if `values` is sorted then
-/// the permutations are lexicographically sorted).
-///
-/// The total number of permutations produced is `len(values)`!. If
-/// `values` contains repeated elements, then some permutations are
-/// repeated.
-///
-/// To avoid memory allocations and copying, the iterator will be
-/// passed a reference to a vector containing references to the
-/// elements in the original `values` vector.
-///
-/// # Arguments
-///
-/// * `values` - A vector of values from which the permutations are
-/// chosen
-///
-/// * `fun` - The function to iterate over the combinations
-pub fn each_permutation<T : Clone>(values : &[T], fun : |perm:&[T]| -> bool) {
-    let length = values.len();
-    let mut permutation = vec::from_fn(length, |i| values[i].clone());
-    if length <= 1 {
-        fun(permutation);
-        return;
-    }
-    let mut indices = vec::from_fn(length, |i| i);
-    loop {
-        if !fun(permutation) { return; }
-        // find largest k such that indices[k] < indices[k+1]
-        // if no such k exists, all permutations have been generated
-        let mut k = length - 2;
-        while k > 0 && indices[k] >= indices[k+1] {
-            k -= 1;
-        }
-        if k == 0 && indices[0] > indices[1] { return; }
-        // find largest l such that indices[k] < indices[l]
-        // k+1 is guaranteed to be such
-        let mut l = length - 1;
-        while indices[k] >= indices[l] {
-            l -= 1;
-        }
-        // swap indices[k] and indices[l]; sort indices[k+1..]
-        // (they're just reversed)
-        // indices[k] <-> indices[l];
-        // vec::swap(indices, k, l);
-        indices.swap(k,l);
-        reverse_part(indices, k+1, length);
-        // fixup permutation based on indices
-        for i in range(k, length) {
-            permutation[i] = values[indices[i]].clone();
+ /// Iterate over all permutations of vector `values`.
+ ///
+ /// Permutations are produced in lexicographic order with respect to
+ /// the order of elements in `values` (so if `values` is sorted then
+ /// the permutations are lexicographically sorted).
+ ///
+ /// The total number of permutations produced is `len(values)`!. If
+ /// `values` contains repeated elements, then some permutations are
+ /// repeated.
+ ///
+ /// To avoid memory allocations and copying, the iterator will be
+ /// passed a reference to a vector containing references to the
+ /// elements in the original `values` vector.
+ ///
+ /// # Arguments
+ ///
+ /// * `values` - A vector of values from which the permutations are
+ /// chosen
+ ///
+ /// * `fun` - The function to iterate over the combinations
+ pub fn each_permutation<T : Clone>(values : &[T], fun : |perm:&[T]| -> bool) {
+     let length = values.len();
+     let mut permutation = Vec::from_fn(length, |i| values[i].clone());
+     if length <= 1 {
+         fun(permutation.as_slice());
+         return;
+     }
+     let mut indices = Vec::from_fn(length, |i| i);
+     loop {
+         if !fun(permutation.as_slice()) { return; }
+         // find largest k such that indices[k] < indices[k+1]
+         // if no such k exists, all permutations have been generated
+         let mut k = length - 2;
+         while k > 0 && *indices.get(k) >= *indices.get(k+1) {
+             k -= 1;
+         }
+         if k == 0 && *indices.get(0) > *indices.get(1) { return; }
+         // find largest l such that indices[k] < indices[l]
+         // k+1 is guaranteed to be such
+         let mut l = length - 1;
+         while *indices.get(k) >= *indices.get(l) {
+             l -= 1;
+         }
+         // swap indices[k] and indices[l]; sort indices[k+1..]
+         // (they're just reversed)
+         // indices[k] <-> indices[l];
+         // vec::swap(indices, k, l);
+         indices.as_mut_slice().swap(k,l);
+         reverse_part(indices.as_mut_slice(), k+1, length);
+         // fixup permutation based on indices
+         for i in range(k, length) {
+             *permutation.get_mut(i) = values[*indices.get(i)].clone();
         }
     }
 }
@@ -195,13 +192,14 @@ pub fn each_permutation<T : Clone>(values : &[T], fun : |perm:&[T]| -> bool) {
 ///
 /// * `fun` - The function to iterate over the permutations
 pub fn each_permutation_ref<'v,T>(values : &'v [T], fun : |perm:&[&'v T]| -> bool) {
-    each_permutation(vec::from_fn(values.len(), |i| &values[i]), fun);
+    let v = Vec::from_fn(values.len(), |i| &values[i]);
+    each_permutation(v.as_slice(), fun);
 }
 
 #[cfg(test)]
 mod tests {
 
-    fn dup<T:Clone>(values : &[&T]) -> ~[T] {
+    fn dup<T:Clone>(values : &[&T]) -> Vec<T> {
         vec::from_fn(values.len(), |i| *values[i])
     }
 
@@ -215,7 +213,7 @@ mod tests {
     #[test]
     fn test_zero() {
         let values = [1,2,3,4];
-        let mut v : ~[~[int]] = ~[];
+        let mut v : Vec<Vec<int>> = Vec::new();
         each_combination(values,0, |p| { v.push(vec::from_slice(p)); });
         assert!(v == ~[]);
     }
