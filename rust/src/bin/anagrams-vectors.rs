476c6a944a546d0de8515f9c09f9cc5e0d119b86
Tommy M. McGuire <mcguire@crsr.net>
2015-04-16 14:53:11 -0500

    updating to 1.0.0-beta: anagrams-vectors

--- rust/src/bin/anagrams-vectors.rs
+++ rust/src/bin/anagrams-vectors.rs
@@ -1,47 +1,49 @@
 extern crate combinations;
 extern crate bisect;
 
-use std::os;
-use std::io::{File,BufferedReader};
-use std::collections::hashmap::HashSet;
+use std::default::Default;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead,BufReader,Read};
+use std::path::Path;
 
-fn split_words(s : &str) -> Vec<String> { s.words().map(|w| w.to_string()).collect() }
+fn split_words(s : &str) -> Vec<String> { s.split(" ").map(|w| w.to_string()).collect() }
 
 fn load_dictionary() -> (Vec<Vec<char>>,Vec<Vec<String>>) {
-    let mut bufferedFile = BufferedReader::new( File::open( &Path::new("anadict.txt") ) );
+    let buffered_file = BufReader::new( File::open( &Path::new("anadict.txt") ).unwrap() );
     let mut keys = Vec::new();
     let mut values = Vec::new();
-    for line in bufferedFile.lines() {
+    for line in buffered_file.lines() {
         let line = line.unwrap();
-        let words = split_words(line.as_slice());
-        keys.push( words.get(0).as_slice().chars().collect() );
+        let words = split_words(&line);
+        keys.push( words[0].chars().collect() );
         values.push( words.iter().skip(1).map(|w| w.clone()).collect() );
     }
-    return (keys,values);
+    (keys,values)
 }
 
 fn get_letters(s : &str) -> Vec<char> {
     let mut t : Vec<char> = s.chars().collect();
     t.sort();
-    return t;
+    t
 }
 
 pub fn main() {
-    let args = os::args();
-    if args.len() < 2 { fail!("Usage: anagrams letters"); }
-    let letters = get_letters(args.get(1).as_slice());
+    let args : Vec<String> = std::env::args().collect();
+    if args.len() < 2 { panic!("Usage: anagrams letters"); }
+    let letters = get_letters(&args[1]);
     let (keys,values) = load_dictionary();
     let klen = keys.len();
     let mut set = HashSet::new();
-    for i in range(2u, letters.len() + 1) {
-        let mut key = Vec::from_elem(i, '\x00');
-        combinations::each_combination(letters.as_slice(), i, |combo| {
-            for j in range(0, combo.len()) { *key.get_mut(j) = combo[j]; }
-            let j = bisect::bisect_left_ref(keys.as_slice(), &key, 0, klen);
-            if j < klen && *keys.get(j) == key {
-                for word in values.get(j).iter() { set.insert(word.clone()); }
+    for i in 2..letters.len() + 1 {
+        let mut key : Vec<char> = vec![Default::default(); i];
+        combinations::each_combination(&letters, i, |combo| {
+            for j in 0..combo.len() { key[j] = combo[j]; }
+            let j = bisect::bisect_left_ref(&keys, &key, 0, klen);
+            if j < klen && keys[j] == key {
+                for word in values[j].iter() { set.insert(word.clone()); }
             }
         });
     }
-    println!("{:u}", set.len());
+    println!("{}", set.len());
 }
