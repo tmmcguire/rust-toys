4e752f70c57ac3583aaa46631b14380348e240df
Tommy M. McGuire <mcguire@crsr.net>
2016-01-27 17:16:41 -0600

    * move anagrams-vectors-tasks for updating * update to Rust 1.6

--- rust/src/bin/anagrams-vectors-tasks.rs
+++ rust/src/bin/anagrams-vectors-tasks.rs
@@ -1,22 +1,24 @@
 extern crate combinations;
 extern crate bisect;
 
-use std::io::{BufferedReader,File};
-use std::collections::hashmap::HashSet;
+use std::io::{BufReader,BufRead};
+use std::fs::File;
+use std::collections::HashSet;
+use std::sync::mpsc::{channel,Sender,Receiver};
+use std::thread;
 
-pub fn split_words(s : &str) -> Vec<String> { s.words().map(|w| w.to_string()).collect() }
+pub fn split_words(s : &str) -> Vec<String> { s.split_whitespace().map(|w| w.to_string()).collect() }
 
 fn load_dictionary() -> (Vec<Vec<char>>,Vec<Vec<String>>) {
-    let path = Path::new("anadict.txt");
-    let file = File::open(&path);
-    let mut bufferedFile = BufferedReader::new(file);
+    let file = File::open("anadict.txt").expect("cannot read anadict.txt");
+    let buffered_file = BufReader::new(file);
     let mut keys = Vec::new();
     let mut values = Vec::new();
-    for line in bufferedFile.lines() {
+    for line in buffered_file.lines() {
         let line = line.unwrap();
-        let words = split_words(line.as_slice());
-        keys.push( words.get(0).as_slice().chars().collect() );
-        values.push( Vec::from_fn(words.len() - 1, |i| words.get(i+1).clone()) );
+        let words = split_words(&line);
+        keys.push( words[0].chars().collect() );
+        values.push( (0..words.len() - 1).map(|i| words[i+1].clone() ).collect() );
     }
     return (keys,values);
 }
@@ -33,7 +35,7 @@ fn search(keys         : &[Vec<char>],
     let klen = keys.len();
     let mut set = HashSet::new();
     loop {
-        let key_set = request_port.recv();
+        let key_set = request_port.recv().unwrap();
         if key_set.len() == 0 { break; }
         for key in key_set.iter() {
             let j = bisect::bisect_left_ref(keys, key, 0, klen);
@@ -45,17 +47,17 @@ fn search(keys         : &[Vec<char>],
     return set;
 }
 
-fn spawn_workers(width : uint) -> (Receiver<HashSet<String>>, Vec<Sender<Vec<Vec<char>>>>) {
-    let (response_sender, response_receiver) = std::comm::channel();
+fn spawn_workers(width : usize) -> (Receiver<HashSet<String>>, Vec<Sender<Vec<Vec<char>>>>) {
+    let (response_sender, response_receiver) = channel();
     let mut request_senders : Vec<Sender<Vec<Vec<char>>>> = Vec::new();
-    for _ in range(0, width) {
-        let (request_chan,request_port) = std::comm::channel();
+    for _ in 0..width {
+        let (request_chan,request_port) = channel();
         request_senders.push(request_chan);
         // Set up and start worker task
         let response_sender = response_sender.clone();
-        std::task::spawn (proc() {
+        thread::spawn (move || {
             let (keys,values) = load_dictionary();
-            response_sender.send( search(keys.as_slice(), values.as_slice(), &request_port) );
+            response_sender.send( search(&keys, &values, &request_port) ).unwrap();
         });
     }
     (response_receiver, request_senders)
@@ -65,35 +67,35 @@ fn main() {
     let width = 6;              // number of worker tasks
     let depth = 500000;         // keys / request sent to worker task
 
-    let args = std::os::args();
+    let mut args = std::env::args();
     if args.len() < 2 {
-        fail!("Usage: anagrams letters");
+        panic!("Usage: anagrams letters");
     }
-    let letters = get_letters(args.get(1).as_slice());
+    let letters = get_letters(&args.nth(1).unwrap());
 
     let (response_receiver, request_senders) = spawn_workers(width);
 
     let mut t = 0;
     let mut key_set = Vec::new();
-    for i in range(2u,letters.len() + 1) {
-        combinations::each_combination(letters.as_slice(), i, |combo| {
+    for i in 2..letters.len() + 1 {
+        combinations::each_combination(&letters, i, |combo| {
             key_set.push( combo.to_owned() );
             if key_set.len() >= depth {
                 let mut ks = Vec::new();
                 // ks <-> key_set;
                 std::mem::swap(&mut ks, &mut key_set);
-                request_senders.get(t).send(ks);
+                request_senders.get(t).unwrap().send(ks).unwrap();
                 t = (t + 1) % width;
             }
         });
     }
-    if !key_set.is_empty() { request_senders.get(t).send(key_set); }
-    for sender in request_senders.iter() { sender.send(Vec::new()) };
+    if !key_set.is_empty() { request_senders.get(t).unwrap().send(key_set).unwrap(); }
+    for sender in request_senders.iter() { sender.send(Vec::new()).unwrap(); };
 
     let mut set : HashSet<String> = HashSet::new();
-    for _ in range(0, width) {
-        let response_set = response_receiver.recv();
+    for _ in 0..width {
+        let response_set = response_receiver.recv().unwrap();
         for word in response_set.iter() { set.insert(word.clone()); }
     }
-    println!("{:u}", set.len());
+    println!("{}", set.len());
 }
